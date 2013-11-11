e89174b99bad176a370d64388bd47a53a71fbee0
Artur Czepiel <czepiel.artur@gmail.com>
2013-11-11 06:23:02 +0100

    nazwa_wyswietlana => display_name
    Signed-off-by: Artur Czepiel <czepiel.artur@gmail.com>

--- apps/userstuff/models.py
+++ apps/userstuff/models.py
@@ -5,7 +5,7 @@ from helpers.helpers import generate_salt
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
-    nazwa_wyswietlana = models.CharField(max_length=30)
+    display_name = models.CharField(max_length=30)
     current_salt = models.CharField(max_length=50)
 
     def __unicode__(self):
@@ -16,9 +16,6 @@ class UserProfile(models.Model):
 
     def get_wklejki(self):
         return self.user.wklejka_set
-
-    def get_liczba_wklejek(self):
-        return self.get_wklejki().count()
 
     def generate_new_salt(self):
         new_salt = generate_salt()
