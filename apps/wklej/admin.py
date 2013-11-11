7c80dabcfc885808f1b3177a7a915687a5218544
Artur Czepiel <czepiel.artur@gmail.com>
2013-11-11 06:28:15 +0100

    Removing tags field from WklejAdmin.search_fields
    Signed-off-by: Artur Czepiel <czepiel.artur@gmail.com>

--- apps/wklej/admin.py
+++ apps/wklej/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 
 class WklejAdmin(admin.ModelAdmin):
     list_display = ['user', 'nickname', 'syntax', 'is_private']
-    search_fields = ['user', 'nickname', 'comment', 'tags', 'body', 'Ip']
+    search_fields = ['user', 'nickname', 'comment', 'body', 'ip']
     list_filter = ['is_private', 'is_deleted', 'is_spam']
     raw_id_fields = ['parent', ]
 
